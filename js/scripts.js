41eeff2ee17392e0cf0f63b893a24f32c4f540ea
stbugyei <stbugyei@yahoo.co.uk>
2020-08-11 11:30:28 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
 console.log('That was it');
 
 
+let a = 1;
+let b = 2;
+let c = a + b
+console.log(a + b)
+console.log(c)
+
+if (a < b) {
+    console.log('a is greater than b')
+    console.log(a + ' is greater than ' + b)
+}
 
